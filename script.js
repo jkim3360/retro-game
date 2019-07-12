bec01d172a08645b1524a712d4c8ebd31d1b03bf
James Kim <daeho@Daeho.fios-router.home>
2019-07-11 23:30:14 -0400

    added more imgs, music, healthbar

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 /* eslint-disable */
 
+
+
+//index.html landing
+const messageIndex = document.querySelector('.messageIndex')
+var trumpPic = document.getElementById('trumpPic')
+
+setTimeout(() => { messageIndex.innerHTML = 'hurry, before the music ends' }, 4000)
+setTimeout(() => { messageIndex.innerHTML = "show everyone you're not a n00b" }, 8000)
+setTimeout(() => { messageIndex.innerHTML = 'if u dunno wat a n00b is' }, 12000)
+setTimeout(() => { messageIndex.innerHTML = "it's because ur a n00b" }, 16000)
+setTimeout(() => { messageIndex.innerHTML = "lol" }, 18000)
+setTimeout(() => { messageIndex.innerHTML = "lawl" }, 20000)
+setTimeout(() => { messageIndex.innerHTML = "LOL" }, 22000)
+setTimeout(() => { messageIndex.innerHTML = "aite" }, 24000)
+setTimeout(() => { messageIndex.innerHTML = "just kidding" }, 27000)
+setTimeout(() => { messageIndex.innerHTML = "u can do it" }, 30000)
+setTimeout(() => { messageIndex.innerHTML = "let's go!!" }, 33000)
+
+
+//main.html
+const pikachu = document.querySelector('.pikachu')
 const dodger = document.querySelector('.dodger');
 const star = document.querySelector('.star');
 let trump = document.querySelector('.trump');
 const sword = document.querySelector('.sword');
+const sky = document.querySelector('.sky')
+const message = document.querySelector('.message')
+// const heart = document.querySelector('.heart')
+
+
+// for (let i = 0; i < 3; i++) {
+// function takeHeart() {
+//   document.querySelector('.heart')
+//   const heartDiv = document.createElement('div')
+//   heartDiv.className = 'heart'
+//   document.querySelector('.sky').appendChild(heartDiv)
+// }
+// takeHeart()
+// }
+
+let enemies = [trump, sword, star]
 
 //dodger movement
-let mouseX = 0;
-let mouseY = 0;
+let mouseX = 150;
+let mouseY = 250;
 let dodgerX = 0;
 let dodgerY = 0;
-const vel = 0.7;
+const vel = 1;
 
 function animate() {
   const distX = mouseX - dodgerX;
@@ -42,7 +79,6 @@ document.addEventListener('click', (event) => {
   }
   turnLeft();
 });
-
 
 // prevent default
 const keys = {
@@ -96,12 +132,13 @@ window.onload = function () {
   startTimer(zero, display);
 };
 
-
 //score
 const score = document.getElementById('scoreId').textContent;
 // get the starting score '0000'
 let intScore = parseInt(score);
 // sets score str to integer
+
+
 
 // const time = document.getElementById('time').textContent;
 setInterval(() => {
@@ -119,106 +156,167 @@ setInterval(() => {
   }
 }, 500);
 
+// sound effects-----------------------------------------------------------------
 
+function Sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  }
+  this.stop = function () {
+    this.sound.pause();
+  }
+}
+
+const soundHit = new Sound('sounds/spring.wav')
+//https://www.sounds-resource.com/snes/
 // -----------------------------------------------------------------
-
-
-
-
-dodger.style.top = "0px";
-dodger.style.height = "100px";
-trump.style.top = "500px";
-trump.style.height = "60px";
-sword.style.top = "500px";
-sword.style.height = "-100px";
-star.style.top = "25px";
-star.style.height = "10%px";
 
 let dodgerStyles = window.getComputedStyle(dodger)
 let trumpStyles = window.getComputedStyle(trump)
 let swordStyles = window.getComputedStyle(sword)
 let starStyles = window.getComputedStyle(star)
+// let heartStyles = window.getComputedStyle(heart)
+
+dodger.style.top = "0px";
+dodger.style.height = "100px";
+trump.style.top = "400px";
+trump.style.height = "60px";
+sword.style.top = "500px";
+sword.style.height = "-100px";
+star.style.top = "25px";
+star.style.height = "10%px";
+// heart.style.top = "25px";
+// heart.style.height = "10%px";
+
 
 //For every 33ms (about 30fps)
-const hit = setInterval(function () {
+setInterval(function () {
+  //Define boundaries
 
-  //Get the height and position of the player
   var dodgerTop = parseInt(dodgerStyles.top),
-  dodgerHeight = parseInt(dodgerStyles.height);
+    dodgerHeight = parseInt(dodgerStyles.height);
   dodgerLeft = parseInt(dodgerStyles.left);
   dodgerWidth = parseInt(dodgerStyles.width);
 
-  //and the top of the ground
+
+
   var trumpTop = parseInt(trump.style.top),
-  trumpHeight = parseInt(trump.style.height);
+    trumpHeight = parseInt(trump.style.height);
   trumpLeft = parseInt(trumpStyles.left);
   trumpWidth = parseInt(trumpStyles.width);
 
   var swordTop = parseInt(swordStyles.top),
-  swordHeight = parseInt(swordStyles.height);
+    swordHeight = parseInt(swordStyles.height);
   swordLeft = parseInt(swordStyles.left);
   swordWidth = parseInt(swordStyles.width);
 
   var starTop = parseInt(starStyles.top),
-  starHeight = parseInt(starStyles.height);
+    starHeight = parseInt(starStyles.height);
   starLeft = parseInt(starStyles.left);
   starWidth = parseInt(starStyles.width);
-
-  //linear gravity? Why now?
-  trumpTop -= 1
-
   // If character gets hit:
+
+  // var heartTop = parseInt(heartStyles.top),
+  //   heartHeight = parseInt(heartStyles.height);
+  // heartLeft = parseInt(heartStyles.left);
+  // heartWidth = parseInt(heartStyles.width);
+
 
   if ((dodgerTop + dodgerHeight > trumpTop) && (dodgerLeft + dodgerWidth > trumpLeft)
     && (dodgerLeft < trumpLeft + trumpWidth) && (dodgerTop < trumpTop + trumpHeight)) {
-    setTimeout(() => { document.querySelector('.dodger').classList.toggle('flash') }, 1000)
-    document.querySelector('.dodger').classList.toggle('flash')
-    setTimeout(() => { document.querySelector('.sky').removeChild(trump) }, 15)
+    soundHit.play()
+    let health = document.getElementById("health")
+    health.value -= 33;
+    setTimeout(() => { dodger.classList.toggle('flash') }, 1000)
+    dodger.classList.toggle('flash')
+    setTimeout(() => { sky.removeChild(trump) }, 15)
     intScore -= 5
-    setTimeout(function( ){
-    let newTrump = document.createElement('div')
-    newTrump.className = 'trump'
-    document.querySelector('.sky').appendChild(newTrump)}, 2000)
+    setTimeout(() => { message.innerHTML = 'srsly? make America \n great again?' }, 100)
+    setTimeout(() => { message.innerHTML = 'u noob' }, 3000)
+    setTimeout(() => { message.innerHTML = "i don't even know" }, 3000)
+    setTimeout(function () {
+      let newTrump = document.createElement('div')
+      newTrump.className = 'trump'
+      sky.appendChild(newTrump)
+    }, 2000)
   }
+
 
   if ((dodgerTop + dodgerHeight > swordTop) && (dodgerLeft + dodgerWidth > swordLeft)
-  && (dodgerLeft < swordLeft + swordWidth) && (dodgerTop < swordTop + swordHeight)) {
-  setTimeout(() => { document.querySelector('.dodger').classList.toggle('flash') }, 1000)
-  document.querySelector('.dodger').classList.toggle('flash')
-  setTimeout(() => { document.querySelector('.sky').removeChild(sword) }, 15)
-  intScore -= 5
-}
+    && (dodgerLeft < swordLeft + swordWidth) && (dodgerTop < swordTop + swordHeight)) {
+    soundHit.play()
+    health.value -= 33
+    setTimeout(() => { dodger.classList.toggle('flash') }, 200)
+    dodger.classList.toggle('flash')
+    setTimeout(() => { sky.removeChild(sword) }, 5)
+    intScore -= 5
+    setTimeout(() => { message.innerHTML = 'dude, your score..' }, 100)
+    setTimeout(() => { message.innerHTML = "u noob" }, 4000)
+  }
 
   if ((dodgerTop + dodgerHeight > starTop) && (dodgerLeft + dodgerWidth > starLeft)
-  && (dodgerLeft < starLeft + starWidth) && (dodgerTop < starTop + starHeight)) {
-  setTimeout(() => { document.querySelector('.dodger').classList.toggle('flash') }, 1000)
-  document.querySelector('.dodger').classList.toggle('flash')
-  setTimeout(() => { document.querySelector('.sky').removeChild(star) }, 15)
-  intScore -= 5
+    && (dodgerLeft < starLeft + starWidth) && (dodgerTop < starTop + starHeight)) {
+    soundHit.play()
+    health.value -= 33
+    setTimeout(() => { dodger.classList.toggle('flash') }, 1000)
+    dodger.classList.toggle('flash')
+    setTimeout(() => { sky.removeChild(star) }, 15)
+    intScore -= 5
+    setTimeout(() => { message.innerHTML = "you're skillz are basic" }, 100)
   }
+
+
 
   // setInterval(function(){console.log(dodger.style.top)}, 250)
 
   //Set the character's final position    
-  dodger.style.top = dodgerTop + "px";
-  dodger.style.left = dodgerLeft + "px";
-  dodger.style.width = dodgerWidth + "px";
-  dodger.style.height = dodgerHeight + "px";
-  trump.style.top = trumpTop + 'px'
-  trump.style.left = trumpLeft + 'px'
-  trump.style.width = trumpWidth + 'px'
-  trump.style.height = trumpHeight + 'px'
-  sword.style.top = swordTop + 'px'
-  sword.style.left = swordLeft + 'px'
-  sword.style.width = swordWidth + 'px'
-  sword.style.height = swordHeight + 'px'
-  star.style.top = starTop + 'px'
-  star.style.left = starLeft + 'px'
-  star.style.width = starWidth + 'px'
-  star.style.height = starHeight + 'px'
 
+  function position(element, elementTop, elementLeft, elementWidth, elementHeight) {
+    element.style.top = elementTop + "px";
+    element.style.left = elementLeft + "px";
+    element.style.height = elementHeight + "px";
+    element.style.width = elementWidth + "px";
+  }
+
+  position(dodger, dodgerTop, dodgerLeft, dodgerWidth, dodgerHeight)
+  // position(trump, trumpTop, trumpLeft, trumpWidth, trumpHeight)
+  // position(sword, swordTop, swordLeft, swordWidth, swordHeight)
+  // position(star, starTop, starLeft, starWidth, starHeight)
 
 }, 200);
+
+const lose = function () {
+  if (health.value < 100) {
+    alert('you lose')
+    clearInterval(lose)
+  }
+}
+lose()
+  // dodger.style.top = dodgerTop + "px";
+  // dodger.style.left = dodgerLeft + "px";
+  // dodger.style.width = dodgerWidth + "px";
+  // dodger.style.height = dodgerHeight + "px";
+  // trump.style.top = trumpTop + 'px'
+  // trump.style.left = trumpLeft + 'px'
+  // trump.style.width = trumpWidth + 'px'
+  // trump.style.height = trumpHeight + 'px'
+  // sword.style.top = swordTop + 'px'
+  // sword.style.left = swordLeft + 'px'
+  // sword.style.width = swordWidth + 'px'
+  // sword.style.height = swordHeight + 'px'
+  // star.style.top = starTop + 'px'
+  // star.style.left = starLeft + 'px'
+  // star.style.width = starWidth + 'px'
+  // star.style.height = starHeight + 'px'
+
+
+
 
 
 
