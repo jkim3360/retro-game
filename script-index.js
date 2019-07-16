81405d4cd49d21c77bb5a1aa275bdb09833bea18
James Kim <root@Daeho.fios-router.home>
2019-07-16 00:25:35 -0400

    styling updates 2

--- script-index.js
+++ script-index.js
@@ -9,47 +9,47 @@ const name = document.getElementById('nameInput')
 const btn = document.getElementById('button')
 
 btn.addEventListener('click', function () {
-    localStorage.setItem('Game ID', name.value)
-    window.location.href = "main.html"
+  localStorage.setItem('Game ID', name.value)
+  window.location.href = "main.html"
 })
 
 document.getElementById('nameInputForm').addEventListener('keypress', function (event) {
-    if (event.keyCode === 13) {
-        localStorage.setItem('Game ID', name.value)
-        window.location.href = "main.html"
-        event.preventDefault();
-        return false
-    }
+  if (event.keyCode === 13) {
+    localStorage.setItem('Game ID', name.value)
+    window.location.href = "main.html"
+    event.preventDefault();
+    return false
+  }
 })
 
 function disableScroll() {
-    if (window.addEventListener) // older FF
-    { window.addEventListener('DOMMouseScroll', preventDefault, false); }
-    document.addEventListener('wheel', preventDefault, { passive: false }); // Disable scrolling in Chrome
-    window.onwheel = preventDefault; // modern standard
-    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-    window.ontouchmove = preventDefault; // mobile
-    document.onkeydown = preventDefaultForScrollKeys;
-  }
-  disableScroll();
+  if (window.addEventListener) // older FF
+  { window.addEventListener('DOMMouseScroll', preventDefault, false); }
+  document.addEventListener('wheel', preventDefault, { passive: false }); // Disable scrolling in Chrome
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove = preventDefault; // mobile
+  document.onkeydown = preventDefaultForScrollKeys;
+}
+disableScroll();
 
-  const keys = {
-    37: 1, 38: 1, 39: 1, 40: 1,
-  };
-  
-  function preventDefault(event) {
-    event = window.event;
-    if (event.preventDefault) event.preventDefault();
-    e.returnValue = false;
+const keys = {
+  37: 1, 38: 1, 39: 1, 40: 1,
+};
+
+function preventDefault(event) {
+  event = window.event;
+  if (event.preventDefault) event.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(event) {
+  if (keys[event.keyCode]) {
+    preventDefault(event);
+    return false;
   }
-  
-  function preventDefaultForScrollKeys(event) {
-    if (keys[event.keyCode]) {
-      preventDefault(event);
-      return false;
-    }
-  }
-  disableScroll();
+}
+disableScroll();
 
 
 setTimeout(() => { messageIndex.innerHTML = 'hurry, before the music ends' }, 4000)
